17a9b88820d5911e6f1c39193e41eb8e641bfd20
Ilmir Usmanov <ilmir.usmanov@jetbrains.com>
2018-01-23 20:35:50 +0300

    Move coroutineContext to correct package
    from kotlin.coroutines.experimental.instrinsics to kotlin.coroutines.experimental
    
     #KT-22400

--- compiler/testData/diagnostics/testsWithStdLib/coroutines/coroutineContext.kt
+++ compiler/testData/diagnostics/testsWithStdLib/coroutines/coroutineContext.kt
@@ -1,31 +1,37 @@
 // !DIAGNOSTICS: -UNUSED_PARAMETER
+// SKIP_TXT
 
-import kotlin.coroutines.experimental.intrinsics.coroutineContext
 import kotlin.coroutines.experimental.CoroutineContext
 
 fun ordinal() {
-    <!ILLEGAL_SUSPEND_PROPERTY_ACCESS!>coroutineContext<!>
+    kotlin.coroutines.experimental.intrinsics.<!DEPRECATION, ILLEGAL_SUSPEND_PROPERTY_ACCESS!>coroutineContext<!>
+    kotlin.coroutines.experimental.<!ILLEGAL_SUSPEND_PROPERTY_ACCESS!>coroutineContext<!>
 }
 
 suspend fun named() {
-    coroutineContext
+    kotlin.coroutines.experimental.intrinsics.<!DEPRECATION!>coroutineContext<!>
+    kotlin.coroutines.experimental.coroutineContext
 }
 
 class A {
-    val coroutineContext = kotlin.coroutines.experimental.intrinsics.<!ILLEGAL_SUSPEND_PROPERTY_ACCESS!>coroutineContext<!>
+    val coroutineContextOld = kotlin.coroutines.experimental.intrinsics.<!DEPRECATION, ILLEGAL_SUSPEND_PROPERTY_ACCESS!>coroutineContext<!>
+    val coroutineContextNew = kotlin.coroutines.experimental.<!ILLEGAL_SUSPEND_PROPERTY_ACCESS!>coroutineContext<!>
 }
 
 class Controller {
     fun ordinal() {
-        <!ILLEGAL_SUSPEND_PROPERTY_ACCESS!>coroutineContext<!>
+        kotlin.coroutines.experimental.intrinsics.<!DEPRECATION, ILLEGAL_SUSPEND_PROPERTY_ACCESS!>coroutineContext<!>
+        kotlin.coroutines.experimental.<!ILLEGAL_SUSPEND_PROPERTY_ACCESS!>coroutineContext<!>
     }
 
     suspend fun named() {
-        coroutineContext
+        kotlin.coroutines.experimental.intrinsics.<!DEPRECATION!>coroutineContext<!>
+        kotlin.coroutines.experimental.coroutineContext
     }
 
     suspend fun severalArgs(s: String, a: Any) {
-        coroutineContext
+        kotlin.coroutines.experimental.intrinsics.<!DEPRECATION!>coroutineContext<!>
+        kotlin.coroutines.experimental.coroutineContext
     }
 }
 
@@ -36,8 +42,12 @@ fun builderSeveralArgs(c: (Int, Int, Int) -> CoroutineContext) = {}
 fun builderSuspendSeveralArgs(c: suspend (Int, Int, Int) -> CoroutineContext) = {}
 
 fun test() {
-    builder { <!ILLEGAL_SUSPEND_PROPERTY_ACCESS!>coroutineContext<!> }
-    builderSuspend { coroutineContext }
-    builderSeveralArgs {_, _,_ -> <!ILLEGAL_SUSPEND_PROPERTY_ACCESS!>coroutineContext<!> }
-    builderSuspendSeveralArgs {_, _,_ -> coroutineContext}
+    builder { kotlin.coroutines.experimental.intrinsics.<!DEPRECATION, ILLEGAL_SUSPEND_PROPERTY_ACCESS!>coroutineContext<!> }
+    builder { kotlin.coroutines.experimental.<!ILLEGAL_SUSPEND_PROPERTY_ACCESS!>coroutineContext<!> }
+    builderSuspend { kotlin.coroutines.experimental.intrinsics.<!DEPRECATION!>coroutineContext<!> }
+    builderSuspend { kotlin.coroutines.experimental.coroutineContext }
+    builderSeveralArgs {_, _,_ -> kotlin.coroutines.experimental.intrinsics.<!DEPRECATION, ILLEGAL_SUSPEND_PROPERTY_ACCESS!>coroutineContext<!> }
+    builderSeveralArgs {_, _,_ -> kotlin.coroutines.experimental.<!ILLEGAL_SUSPEND_PROPERTY_ACCESS!>coroutineContext<!> }
+    builderSuspendSeveralArgs {_, _,_ -> kotlin.coroutines.experimental.intrinsics.<!DEPRECATION!>coroutineContext<!>}
+    builderSuspendSeveralArgs {_, _,_ -> kotlin.coroutines.experimental.coroutineContext}
 }
